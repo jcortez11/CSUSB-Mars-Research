84d8def6e7039452b56f011f00f5a8f15c91aa38
Marek Slipski <marek.slipski@colorado.edu>
2021-11-08 11:30:36 -0800

    ice/null model in notebook

--- train.py
+++ train.py
@@ -19,7 +19,8 @@ def train(data):
     x = data.drop(columns=["target"])
     y = data["target"]
     x.replace([np.inf, -np.inf], np.nan, inplace=True)  # change infs to NaNs
-    x.fillna(0, inplace=True)  # drop any NaNs for training
+    # Should be dropna?
+    # x.fillna(0, inplace=True)  # drop any NaNs for training
     scaler = sklearn.preprocessing.MinMaxScaler()  # setup Normalization
     x = scaler.fit_transform(x)  # normalize data
     #lr = LR()
